954918575db50b3385338133de5b0281ad2fd912
S1monG <si5564gu-s@student.lu.se>
2022-11-30 11:54:38 +0100

    Continue working on js script.

---
+++ script.js
@@ -0,0 +1,11 @@
+/* var i = 0;
+var txt = "Hello! I'm Simon";
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typing").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+} */
