61dc36fd0a615961ae3e993c0bc34e3a2ea55ba0
S1monG <si5564gu-s@student.lu.se>
2024-07-13 15:13:19 +0200

    Replace BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { FC, ReactElement, useMemo, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import Header from './components/Header';
 import CenteredTabs from './tabs/CenteredTabs';
-import Me from './tabs/Me';
-import API from './tabs/API';
+
 import { PaletteTheme } from './utils/Theme';
 import { getDesignTokens } from './utils/Theme';
 import { ColorModeContext } from './utils/ColorModeContext';
@@ -19,19 +17,10 @@ const App: FC = (): ReactElement => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-
-        <ColorModeContext.Provider value={{ themeMode, setThemeMode }}>
-          <Header title="Okayy lets go" />
-        </ColorModeContext.Provider>
-        <CenteredTabs />
-
-        <Routes>
-          <Route path="/" element={<Me/>} />
-          <Route path="/api" element={<API/>} />
-          <Route path="/cache" element={<div>to be continued</div>} />
-        </Routes>
-      </Router>
+      <ColorModeContext.Provider value={{ themeMode, setThemeMode }}>
+        <Header title="Okayy lets go" />
+      </ColorModeContext.Provider>
+      <CenteredTabs />
     </ThemeProvider>
   )
 }
