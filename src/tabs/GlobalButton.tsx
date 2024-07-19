b4b39f17ee2e6555e3f14eb8ee75b9254c5f6647
S1monG <si5564gu-s@student.lu.se>
2024-07-19 21:09:05 +0200

    finish simple button

--- src/tabs/GlobalButton.tsx
+++ src/tabs/GlobalButton.tsx
@@ -1,10 +1,10 @@
-import {FC, ReactElement, useEffect, useState } from 'react'
-import {Container, Button } from '@mui/material'
+import { FC, ReactElement, useEffect, useState } from 'react'
+import { Button, Typography, Box } from '@mui/material'
 
 
 
 const GlobalButton: FC = (): ReactElement => {
-  const [counter, setCounter] = useState(undefined)
+  const [counter, setCounter] = useState(5)
 
   useEffect(() => {
     (async () => {
@@ -23,9 +23,47 @@ const GlobalButton: FC = (): ReactElement => {
   }
 
   return (
-    <Container>
-      <Button variant="contained" onClick={incrementCounter}>{counter}</Button>
-    </Container>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      height="60vh"
+    >
+      <Box 
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        flexDirection={{xs: 'column', sm: 'column', md: 'row'}}
+        marginBottom={{xs: '15px', sm: '30px'}}
+        gap={{xs: '10px', sm: '20px'}}
+      >
+        <Typography fontWeight='bold' fontSize={{xs: '1.5rem', sm: '2rem', md: '2.5rem'}}>This button has been clicked</Typography>
+        <Typography
+          sx={{
+            fontWeight: 'bold',
+            fontSize: '4rem',
+            background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            MozBackgroundClip: 'text',
+            MozTextFillColor: 'transparent',
+            filter: 'drop-shadow(2px 2px 2px #222)',
+            textAlign: 'center'
+          }}
+        >
+          {counter}
+        </Typography>
+        <Typography fontWeight='bold' fontSize={{xs: '1.5rem', sm: '2rem', md: '2.5rem'}}>times</Typography>
+      </Box>
+      <Button 
+        variant="contained"
+        onClick={incrementCounter}
+        size='large'
+      >
+        Signifier
+      </Button>
+    </Box>
   )
 }
 
